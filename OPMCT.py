b4160dc1e1e057028ce3d70064e38a6f5db1583e
JOELSR <joel.santana@alumnos.uv.cl>
2022-11-09 05:25:54 -0300

    Agregados comentarios

--- OPMCT.py
+++ OPMCT.py
@@ -15,11 +15,13 @@ class OPMCTSystem(CoupledDEVS):
 		# CLASS DISPATCHER
 		dispatcher = Dispatcher()
 
+		#Creación de camiones
 		self.camiones = []
 		for i in range(cQ):
 			name = "camion_"+str(i)
 			self.camiones.append(self.addSubModel(Camion(sQ,pQ,name,dispatcher)))
 
+		#Creación de palas
 		self.palas = []
 		if(palaToStock != []):
 			for i in range(pQ):
@@ -32,12 +34,15 @@ class OPMCTSystem(CoupledDEVS):
 				dispatcher.addPala(name)
 				self.palas.append(self.addSubModel(Pala(name,cQ)))
 
+		#Creación de StockPiles
 		self.stockPiles = []
 		for i in range(sQ):
 			self.stockPiles.append(self.addSubModel(StockPile("stock_"+str(i+1))))
 
+		#Collector para datos
 		self.collector = self.addSubModel(Collector(cQ,pQ))
 
+		#Conexión de puertos para proceso de CARGA y TRANSPORTE
 		for i in range(pQ):
 			for j in range(cQ):
 				self.connectPorts(self.camiones[j].notify["pala_"+str(i)],self.palas[i].CAMION)
@@ -45,6 +50,7 @@ class OPMCTSystem(CoupledDEVS):
 				self.connectPorts(self.palas[i].DATA,self.collector.in_event)
 				self.connectPorts(self.camiones[j].DATA,self.collector.in_event)
 		
+		#Conexión de puertos para proceso de DESCARGA
 		for i in range(cQ):
 			for j in range(sQ):
 				self.connectPorts(self.camiones[i].out_load["stock_"+str(j+1)],self.stockPiles[j].LOAD)
